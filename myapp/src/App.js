b0dd2a43aa9441bde62248a66775cebe642376a9
sfwnisme <sfwnisme@gmail.com>
2023-02-22 11:51:23 +0300

    .g

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,17 +3,70 @@ import Child from './Child'
 // import './App.css';
 
 class App extends Component {
-  test() {
-    console.log("hello")
+  // in the element attribute field it needs to add .bind(this) after the function name
+  state = {
+    name: 'Borada',
+    name2: 'Change me'
   }
+
+  // static function
+  onClick() {
+    console.log('static click function')
+  }
+  // arrow function
+  onClick2 = () => console.log('arrow click function')
+
+  // using state
+  // static function
+  onClick3() {
+    console.log(this.state.name, 'from state')
+  }
+  // arrow function 
+  onClick4 = () => {
+    console.log(this.state.name, ' from state with arrow no .bind(this)')
+  }
+
+
+
+  changeState = () => {
+    this.state.name2 = "new name"
+    this.setState({
+      name2: 'Changed'
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Child/>
-        {this.test()}
+        <Child />
+        {/* using the events normally without any additions */}
+        <button onClick={this.onClick}>Click static</button>
+        <button onClick={this.onClick2}>Click arrow</button>
+
+        {/* using events with state method */}
+        <button onClick={this.onClick3.bind(this)}>Click static with state</button>
+        <button onClick={this.onClick3}>Click static with state no .bind(this)</button>
+        <button onClick={this.onClick4}>Click arrow with state no .bind(this)</button>
+
+        {/* click on this element and will see the changes */}
+        <h2 onClick={this.changeState}>{this.state.name2}</h2>
       </div>
     )
   }
 }
 
 export default App;
+
+
+// ====| EVENTS
+/* So
+* using function static or arrow works normal
+* using static function with state object needs 
+to type like this => (functionName.bind(this))
+#.bind(this) will move on and target the js 
+class the class that contains the function or the process
+* using arrow function with state object will be helpful
+and best option due to you can avoid usnig .bind(this) method 
+SO using arrow function with state is the perfect way
+
+*/
--- myapp/src/Child.js
+++ myapp/src/Child.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
 class Child extends Component {
-  Test() {
-    <p>this is an arrow function</p>
-  }
   render() {
     return (
       <div>
-        React HHHHHHHHHaaai
-        {this.Test()}
+        <h1>Hello from child</h1>
       </div>
     )
   }
