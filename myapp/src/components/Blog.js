597f94bfb526d291da9c852679562f29c3cbf57d
sfwnisme <sfwnisme@gmail.com>
2023-02-27 21:07:01 +0300

    axios

--- myapp/src/components/Blog.js
+++ myapp/src/components/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import './Blog.css'
 
 class Blog extends Component {
 
@@ -18,12 +19,22 @@ class Blog extends Component {
   }
 
   render() {
+    let { users } = this.state;
+    let usersList = users.map((user) => {
+      return (
+        <div key={user.id} className='user-box'>
+          <h3>Name: {user.name}</h3>
+          <p>User: {user.username}</p>
+          <span>Email: {user.email}</span>
+        </div>
+      )
+    })
     return (
       <div>
         <h1>Blog</h1>
         <p>This is About Section</p>
-        <div>
-          {this.state.users.forEach((user) => <div key={user.name}>{user.name} => and his username is {user.username}</div>)}
+        <div className='users'>
+          {usersList}
         </div>
       </div>
     )
